8f02ae82a8198029b0e0e54183c3bb48c4e79262
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-08-29 14:28:57 +0200

    upload initial setup files

---
+++ src/components/Nav.tsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+import '../App.css'
+
+
+export default function Nav() {
+
+  // const [favourites, setFavourites] = useState([])
+
+
+  return (
+    <>
+      <nav className=' flex justify-between items-center pl-2 pr-2 pt-5 pb-2 border-b-2 border-orange-500'>
+        <img src="/src/assets/images/spoonfed-logo.png" alt="Spoonfed logo" width={90}/>
+
+        {/* <div className='flex items-center'>
+          <HeartIcon  className="h-7 w-7" />
+          { favourites.length == 0 ? <p className='text-orange-500 '>0</p> : <p>{favourites.length}</p> }
+        </div> */}
+        
+      </nav>
+    </>
+  )}
