bd699ac3a8a5b3f0a777561c81a7f2ee1dd80422
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-08-29 15:46:53 +0200

    add: nav styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Badge from 'react-bootstrap/Badge'
 import Button from 'react-bootstrap/Button'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
@@ -9,7 +6,7 @@ import './App.css'
 import Nav from './components/Nav'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -19,28 +16,7 @@ function App() {
           Primary
           <i className="bi bi-0-circle-fill"></i>
         </Button>
-        <Badge>
-          New
-        </Badge>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
