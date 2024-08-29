2aaa8b87f1fc4db983e8caa1059e858200ef4c5e
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-08-29 16:56:28 +0200

    comment out unused lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import './App.css'
 import Nav from './components/Nav'
+import QuickFinds from './components/QuickFinds'
+import Search from './components/Search'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -12,10 +14,12 @@ function App() {
     <>
       <div>
         <Nav />
+        <Search />
         <Button variant="danger">
           Primary
           <i className="bi bi-0-circle-fill"></i>
         </Button>
+        <QuickFinds />
       </div>
     </>
   )
